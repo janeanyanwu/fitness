a8a2e488f1b74e4f7bc41f392a0daf7152fef8a7
Jane Anyanwu <122833104+janeanyanwu@users.noreply.github.com>
2023-06-15 13:08:07 +0100

    A fitness or exercise app made with react

---
+++ src/components/Loaders.jsx
@@ -0,0 +1,18 @@
+import { Stack } from "@mui/material";
+import React from "react";
+import { InfinitySpin } from "react-loader-spinner";
+
+const Loaders = () => {
+  return (
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      width="100%"
+    >
+      <InfinitySpin color="gray" />
+    </Stack>
+  );
+};
+
+export default Loaders;
